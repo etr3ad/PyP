873244a535030e40f6d9e74299675ff157000edf
etr3ad <crwanread@gmail.com>
2025-04-18 22:41:47 -0400

    Add files via upload

---
+++ pyproject2.py
@@ -0,0 +1,68 @@
+# Exercise a
+print("Exercise a:")
+a = 1
+b = 2
+c = 5
+while a < c:
+    a = a + 1
+    b = b + c
+print("Output:", a, b, c)
+print()
+
+# Exercise b
+print("Exercise b:")
+d = 4
+e = 6
+f = 7
+while d > f:
+    d = d + 1
+    e = e - 1
+print("Output:", d, e, f)
+print()
+
+# Exercise c
+print("Exercise c:")
+g = 4
+h = 6
+while g < h:
+    g = g + 1
+print("Output:", g, h)
+print()
+
+# Exercise d
+print("Exercise d:")
+j = 2
+k = 5
+n = 9
+while j < k:
+    m = 6
+    while m < n:
+        print("Goodbye")
+        m = m + 1
+    j = j + 1
+print()
+
+# Exercise e
+print("Exercise e:")
+j = 2
+k = 5
+n = 9
+while j < k:
+    m = 6
+    while m < n:
+        print("Hello")
+        m = m + 1
+    j = j + 1
+print()
+
+# Exercise f
+print("Exercise f:")
+p = 2
+q = 4
+while p < q:
+    print("Adios")
+    r = 1
+    while r < q:
+        print("Adios")
+        r = r + 1
+    p = p + 1
